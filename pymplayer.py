bc6f3079a7807aa85202e8ffecb2e3abefa4a2ae
Darwin Bautista <daruuin@gmail.com>
2008-01-08 18:00:35 +0000

    * Finally, MPlayer is now GObject-free! Yay! ;-)   MPlayer now uses asyncore.loop (via poll_output method)   for polling stdout and stderr * Made some methods static (staticmethod) * Server now exposes the MPlayer API and properties:   that is, server.mplayer.command(cmd) -> server.command(cmd) * Added/improved more docstrings

--- pymplayer.py
+++ pymplayer.py
@@ -31,8 +31,6 @@ import asynchat
 from subprocess import Popen, PIPE
 from threading import Thread
 
-import gobject
-
 
 __all__ = ['MPlayer', 'Server', 'Client', 'PORT', 'MAX_CMD_LENGTH']
 
@@ -44,6 +42,41 @@ PORT = 50001
 MAX_CMD_LENGTH = 256
 
 
+class _ReadableFile(object):
+    """Imitates a readable asyncore.dispatcher class.
+
+    This class serves as a wrapper for stdout and stderr
+    so that the polling function of asyncore can check them
+    for any pending I/O events. The polling function will
+    call the handle_read_event method as soon as there is data
+    to read.
+    """
+    def __init__(self, file, map):
+        # Add self to map
+        map[file.fileno()] = self
+
+    def __getattr__(self, attr):
+        # Always return a callable for non-existent attributes.
+        # (IMO, this is better than defining all the other asyncore.dispatcher methods)
+        return lambda: None
+
+    def handle_read_event():
+        """
+        This method is called by the polling function when
+        there is data to read. This will eventually be
+        overridden by MPlayer's _handle_data or _handle_error.
+        """
+        pass
+
+    @staticmethod
+    def readable():
+        return True
+
+    @staticmethod
+    def writable():
+        return False
+
+
 class MPlayer(object):
     """MPlayer(path='mplayer', args=())
 
@@ -51,11 +84,12 @@ class MPlayer(object):
     responses to and from MPlayer. Take note that MPlayer is always
     started in slave (-slave) and idle (-idle) modes.
 
-    The MPlayer process would eventually "freeze" if gobject.MainLoop
-    is not running because the stdout/stderr PIPE buffers would get full.
-
-    The handle_data and handle_error methods would only get executed
-    if gobject.MainLoop is running.
+    WARNING:
+        The MPlayer process would eventually "freeze" if
+        the poll_output method is not called because the
+        stdout/stderr PIPE buffers would get full.
+        Also, the handle_data and handle_error methods
+        would only get executed after poll_output is called.
 
     @property path: path to MPlayer
     @property args: MPlayer arguments
@@ -63,7 +97,8 @@ class MPlayer(object):
     def __init__(self, path='mplayer', args=()):
         self.path = path
         self.args = args
-        self.__subprocess = None
+        self.__process = None
+        self._map = {}
 
     def __del__(self):
         # Be sure to stop the MPlayer process.
@@ -94,33 +129,53 @@ class MPlayer(object):
 
     args = property(_get_args, _set_args, doc="MPlayer arguments")
 
-    def _handle_data(self, source, condition):
-        # source is stdout
-        self.handle_data(source.readline().rstrip())
-        return True
+    def _handle_data(self):
+        self.handle_data(self.__process.stdout.readline().rstrip())
 
-    def _handle_error(self, source, condition):
-        # source is stderr
-        self.handle_error(source.readline().rstrip())
-        return True
+    def _handle_error(self):
+        self.handle_error(self.__process.stderr.readline().rstrip())
+
+    def poll_output(self, timeout=30.0, use_poll=False):
+        """Start asyncore.loop for polling MPlayer's stdout and stderr.
+
+        @param timeout=30.0: timeout parameter for select() or poll()
+        @param use_poll=False: use poll() instead of select()
+
+        This method will block unless it is called BEFORE MPlayer is STARTED
+        or if the MPlayer process is currently NOT running.
+
+        In a multithreaded app, you may want to spawn a new Thread
+        for running this method after calling the start method:
+
+        mplayer = MPlayer()
+        mplayer.start()
+        thread = threading.Thread(target=mplayer.poll_output)
+        thread.setDaemon(True)
+        thread.start()
+        """
+        if not self.isalive() or not self._map:
+            return
+        asyncore.loop(timeout=timeout, use_poll=use_poll, map=self._map)
 
     def start(self):
         """Start the MPlayer process.
 
         Returns True on success, False on failure,
         and None if MPlayer is already running.
+
+        WARNING:
+            Don't forget to run the poll_output method
+            after calling this method.
         """
         if not self.isalive():
             try:
                 # Start subprocess (line-buffered)
-                self.__subprocess = Popen(args=self.__args, stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=1)
+                self.__process = Popen(args=self.__args, stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=1)
             except OSError:
                 retcode = False
             else:
-                self._stdout_watch = gobject.io_add_watch(self.__subprocess.stdout,
-                    gobject.IO_IN|gobject.IO_PRI, self._handle_data)
-                self._stderr_watch = gobject.io_add_watch(self.__subprocess.stderr,
-                    gobject.IO_IN|gobject.IO_PRI, self._handle_error)
+                _ReadableFile(self.__process.stdout, self._map).handle_read_event = self._handle_data
+                _ReadableFile(self.__process.stderr, self._map).handle_read_event = self._handle_error
                 retcode = True
             return retcode
 
@@ -131,9 +186,9 @@ class MPlayer(object):
         """
         if self.isalive():
             self.command("quit")
-            gobject.source_remove(self._stdout_watch)
-            gobject.source_remove(self._stderr_watch)
-            return self.__subprocess.wait()
+            # Clear the map so that asyncore.loop will terminate
+            self._map.clear()
+            return self.__process.wait()
         else:
             return None
 
@@ -150,7 +205,7 @@ class MPlayer(object):
         if not cmd:
             raise ValueError("zero-length command")
         if self.isalive():
-            self.__subprocess.stdin.write("".join([cmd, '\n']))
+            self.__process.stdin.write("".join([cmd, '\n']))
 
     def isalive(self):
         """Check if MPlayer process is alive.
@@ -158,11 +213,12 @@ class MPlayer(object):
         Returns True if alive, else, returns False
         """
         try:
-            return (self.__subprocess.poll() is None)
+            return (self.__process.poll() is None)
         except AttributeError:
             return False
 
-    def handle_data(self, data):
+    @staticmethod
+    def handle_data(data):
         """This method is meant to be overridden.
 
         This method is called when a line is read from stdout.
@@ -171,7 +227,8 @@ class MPlayer(object):
         """
         pass
 
-    def handle_error(self, error):
+    @staticmethod
+    def handle_error(error):
         """This method is meant to be overridden.
 
         This method is called when a line is read from stderr.
@@ -189,16 +246,16 @@ class _Channel(asynchat.async_chat):
     def __init__(self, mplayer, conn, map):
         asynchat.async_chat.__init__(self, conn)
         self.add_channel(map)
-        self.map = map
+        self._map = map
         self.mplayer = mplayer
         self.buffer = []
         self.set_terminator("\r\n\r\n")
 
-    def writable(self):
+    @staticmethod
+    def writable():
         return False
 
     def handle_close(self):
-        self.del_channel(self.map)
         self.close()
         self.log("Connection closed: %s" % (self.addr, ))
 
@@ -211,8 +268,8 @@ class _Channel(asynchat.async_chat):
         if not cmd or _re_cmd_quit.match(cmd):
             self.handle_close()
         elif cmd.lower() == "reload":
-            # (Re)Loading a playlist makes MPlayer "jump out" of its XEmbed container
-            # Restart MPlayer instead
+            # (Re)loading a playlist would make MPlayer "jump out" of
+            # its XEmbed container, restart the MPlayer process instead.
             self.mplayer.stop()
             self.mplayer.start()
         else:
@@ -220,30 +277,48 @@ class _Channel(asynchat.async_chat):
 
 
 class Server(asyncore.dispatcher):
-    """Server(host='', port=pymplayer.PORT, max_conns=1)
+    """Server(host='', port=pymplayer.PORT, max_conn=1)
 
-    The PyMPlayer Server
+    Although this class isn't a subclass of MPlayer, most of
+    the MPlayer API and all MPlayer properties are exposed via
+    the __getattr__ method. The MPlayer properties function and
+    behave properly via some __getattr__ and __setattr__ magic.
 
     The log method can be overridden to provide more sophisticated
     logging and warning methods.
     """
-    def __init__(self, host='', port=PORT, max_conns=1):
-        # Custom socket_map
-        self.socket_map = {}
-        # asyncore.dispatcher is not a new-style class!
-        # Use own socket_map
-        asyncore.dispatcher.__init__(self, map=self.socket_map)
+    def __init__(self, host='', port=PORT, max_conn=1):
+        # Use own socket map
+        self._map = {}
+        asyncore.dispatcher.__init__(self, map=self._map)
         self.host = host
         self.port = port
-        self.max_conns = max_conns
-        # For now, do this since properties can't be used
-        self.mplayer = MPlayer()
+        self.max_conn = max_conn
+        self.__mplayer = MPlayer()
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.set_reuse_addr()
         self.bind((self.host, self.port))
-        self.listen(self.max_conns)
+        self.listen(self.max_conn)
 
-    def writable(self):
+    def __del__(self):
+        self.stop()
+
+    def __getattr__(self, attr):
+        # Expose MPlayer API (except poll_output) and properties
+        if hasattr(self.__mplayer, attr) and attr != 'poll_output':
+            return getattr(self.__mplayer, attr)
+        else:
+            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, attr))
+
+    def __setattr__(self, attr, value):
+        # Make the MPlayer properties behave properly
+        if attr not in ('args', 'path'):
+            self.__dict__[attr] = value
+        else:
+            setattr(self.__mplayer, attr, value)
+
+    @staticmethod
+    def writable():
         return False
 
     def handle_close(self):
@@ -252,33 +327,41 @@ class Server(asyncore.dispatcher):
 
     def handle_accept(self):
         conn, addr = self.accept()
-        if len(self.socket_map) - 1 < self.max_conns:
+        if len(self._map) - 3 < self.max_conn:
             self.log("Connection accepted: %s" % (addr, ))
-            # Dispatch connection to _Channel and set channel logger to self.log
-            _Channel(self.mplayer, conn=conn, map=self.socket_map).log = self.log
+            # Dispatch connection to _Channel and override channel logger
+            _Channel(self.__mplayer, conn=conn, map=self._map).log = self.log
         else:
-            self.log("Max number of connections reached.")
+            self.log("Max number of connections reached, rejected: %s" % (addr, ))
             conn.close()
 
     def stop(self):
         """Stop the server.
 
-        Closes all the channels found in self.socket_map (including itself)
+        Closes all the channels found in self._map (including itself)
         """
-        for channel in self.socket_map.values():
+        for channel in self._map.values():
             channel.handle_close()
-        return self.mplayer.stop()
+        # The _ReadableFile instances would still remain in self._map
+        # Clear map so that asyncore.loop will terminate
+        self._map.clear()
+        return self.__mplayer.stop()
 
-    def start(self, timeout=None):
+    def start(self, timeout=30.0, use_poll=False):
         """Start the server.
+
+        @param timeout=30.0: timeout parameter for select() or poll()
+        @param use_poll=False: use poll() instead of select()
 
         Starts the MPlayer process, then enters asyncore.loop (blocking)
         """
-        if self.mplayer.isalive():
+        if self.__mplayer.isalive():
             return
-        self.mplayer.start()
+        self.__mplayer.start()
+        # Include the _ReadableFile instances from self.__mplayer._map
+        self._map.update(self.__mplayer._map)
         self.log("Server started.")
-        asyncore.loop(timeout=timeout, map=self.socket_map)
+        asyncore.loop(timeout=timeout, use_poll=use_poll, map=self._map)
 
 
 class Client(asynchat.async_chat):
@@ -295,10 +378,12 @@ class Client(asynchat.async_chat):
         self.port = port
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def readable(self):
+    @staticmethod
+    def readable():
         return False
 
-    def handle_connect(self):
+    @staticmethod
+    def handle_connect():
         pass
 
     def handle_error(self):
