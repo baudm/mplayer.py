68c53b0bae150f8551d3de6edada1e0f74923630
Darwin Bautista <daruuin@gmail.com>
2009-02-06 05:20:39 +0000

    stdout/stderr can now be accessed using MPlayer.stdout/stderr: - only fileno() and readline() are exposed - methods for managing callbacks were added a lot other changes...

--- pymplayer.py
+++ pymplayer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # $Id$
 #
-# Copyright (C) 2007-2009  UP EEEI Computer Networks Laboratory
+# Copyright (C) 2007-2008  UP EEEI Computer Networks Laboratory
 # Copyright (C) 2007-2009  Darwin M. Bautista <djclue917@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -25,12 +25,6 @@ MPlayer -- thin, out-of-process wrapper for MPlayer
 Server -- asynchronous server that manages an MPlayer instance
 Client -- client for sending MPlayer commands
 
-Functions:
-
-add_output_watch(mplayer, source, callback) --
-remove_output_watch(mplayer, source) --
-
-
 """
 
 import socket
@@ -47,29 +41,6 @@ __all__ = [
 
 __version__ = '0.4.0'
 __author__ = 'Darwin M. Bautista <djclue917@gmail.com>'
-
-
-STDOUT = 'stdout'
-STDERR = 'stderr'
-
-
-def add_output_watch(fileno, callback):
-    if fileno is not None:
-        _ReadableFile(asyncore.socket_map, fileno, callback)
-        return True
-    else:
-        return False
-
-
-def remove_output_watch(fileno):
-    if fileno is not None:
-        if asyncore.socket_map.has_key(fileno):
-            del asyncore.socket_map[fileno]
-            return True
-        else:
-            return False
-    else:
-        return False
 
 
 class MPlayer(object):
@@ -91,48 +62,39 @@ class MPlayer(object):
     def __init__(self, args=()):
         self.args = args
         self._process = None
-        self._server_data_handlers = {}
+        self._stdout = _File()
+        self._stderr = _File()
 
     def __del__(self):
         # Be sure to stop the MPlayer process.
         self.stop()
 
-    def _call_server_data_handlers(self, data):
-        for handler in self._server_data_handlers.values():
-            handler(data)
+    def _get_stdout(self):
+        return self._stdout
 
-    def _get_stdout_fileno(self):
-        try:
-            return self._process.stdout.fileno()
-        except AttributeError:
-            return None
+    stdout = property(fget=_get_stdout)
 
-    stdout = property(fget=_get_stdout_fileno)
+    def _get_stderr(self):
+        return self._stderr
 
-    def _get_stderr_fileno(self):
-        try:
-            return self._process.stderr.fileno()
-        except AttributeError:
-            return None
-
-    stderr = property(fget=_get_stderr_fileno)
+    stderr = property(fget=_get_stderr)
 
     def _get_args(self):
         return self._args[3:]
 
     def _set_args(self, args):
         if not isinstance(args, (list, tuple)):
-            raise TypeError("args should either be a tuple or list of strings")
+            raise TypeError('args should either be a tuple or list of strings')
         elif args:
             for arg in args:
                 if not isinstance(arg, basestring):
-                    raise TypeError("args should either be a tuple or list of strings")
+                    raise TypeError('args should either be a tuple or list of strings')
         self._args = ['-slave', '-idle', '-quiet']
         self._args.extend(args)
 
-    args = property(_get_args, _set_args, doc="MPlayer arguments")
+    args = property(_get_args, _set_args, doc='MPlayer arguments')
 
-    def start(self, pipe_stdout=False, pipe_stderr=False):
+    def start(self, stdout=None, stderr=None):
         """Start the MPlayer process.
 
         Returns True on success, False on failure,
@@ -142,14 +104,19 @@ class MPlayer(object):
         if not self.isalive():
             args = [self.__class__.bin]
             args.extend(self._args)
-            pipe_stdout = (PIPE if pipe_stdout else None)
-            pipe_stderr = (PIPE if pipe_stderr else None)
+            # Force PIPE if callbacks were added
+            stdout_ = (PIPE if len(self._stdout._callbacks) > 0 else stdout)
+            stderr_ = (PIPE if len(self._stderr._callbacks) > 0 else stderr)
             try:
                 # Start the MPlayer process (line-buffered)
-                self._process = Popen(args=args, stdin=PIPE, stdout=pipe_stdout, stderr=pipe_stderr, bufsize=1)
+                self._process = Popen(args=args, stdin=PIPE, stdout=stdout_, stderr=stderr_, bufsize=1)
             except OSError:
                 return False
             else:
+                if self._process.stdout is not None:
+                    self._stdout._set_file(self._process.stdout)
+                if self._process.stderr is not None:
+                    self._stderr._set_file(self._process.stderr)
                 return True
 
     def stop(self):
@@ -159,9 +126,9 @@ class MPlayer(object):
 
         """
         if self.isalive():
+            self._stdout._unset_file()
+            self._stderr._unset_file()
             self.command('quit')
-            remove_output_watch(self._process.stdout)
-            remove_output_watch(self._process.stderr)
             process = self._process
             self._process = None
             return process.wait()
@@ -203,25 +170,13 @@ class MPlayer(object):
 
         """
         if not isinstance(cmd, basestring):
-            raise TypeError("command must be a string")
+            raise TypeError('command must be a string')
         if self.isalive() and cmd:
-            self._process.stdin.write("".join([cmd, '\n']))
-
-    def readline(self, source):
-        """Read a line from source
-
-        This would also pass the data to the data handlers in
-        self._server_data_handlers
-        """
-        source = getattr(self._process, source)
-        if source is not None:
-            data = source.readline()
-            self._call_server_data_handlers(data)
-            return data
+            self._process.stdin.write(''.join([cmd, '\n']))
 
 
 class Server(asyncore.dispatcher):
-    """Server(host='', port=pymplayer.PORT, max_conn=1)
+    """Server(mplayer, port, host='', max_conn=1)
 
     The log method can be overridden to provide more sophisticated
     logging and warning methods.
@@ -237,7 +192,7 @@ class Server(asyncore.dispatcher):
         self.set_reuse_addr()
         self.bind((host, port))
         self.listen(self._max_conn)
-        self.log("Server started.")
+        self.log('Server started.')
 
     def __del__(self):
         self.stop()
@@ -248,16 +203,16 @@ class Server(asyncore.dispatcher):
 
     def handle_close(self):
         self.close()
-        self.log("Server closed.")
+        self.log('Server closed.')
 
     def handle_accept(self):
         conn, addr = self.accept()
         if len(self._channels) < self._max_conn:
-            self.log("Connection accepted: %s" % (addr, ))
+            self.log('Connection accepted: %s' % (addr, ))
             # Dispatch connection to a _ClientHandler
             _ClientHandler(self._channels, self._mplayer, conn, self.log)
         else:
-            self.log("Max number of connections reached, rejected: %s" % (addr, ))
+            self.log('Max number of connections reached, rejected: %s' % (addr, ))
             conn.close()
 
     def stop(self):
@@ -281,7 +236,7 @@ class Client(asynchat.async_chat):
     def __init__(self):
         asynchat.async_chat.__init__(self)
         self.buffer = []
-        self.set_terminator("\r\n")
+        self.set_terminator('\r\n')
 
     @staticmethod
     def handle_connect():
@@ -289,13 +244,13 @@ class Client(asynchat.async_chat):
 
     def handle_error(self):
         self.close()
-        raise socket.error("Connection lost.")
+        raise socket.error('Connection lost.')
 
     def collect_incoming_data(self, data):
         self.buffer.append(data)
 
     def found_terminator(self):
-        data = "".join(self.buffer)
+        data = ''.join(self.buffer)
         self.buffer = []
         self.handle_data(data)
 
@@ -328,8 +283,8 @@ class Client(asynchat.async_chat):
         http://www.mplayerhq.hu/DOCS/tech/slave.txt
 
         """
-        self.push("".join([cmd, "\r\n\r\n"]))
-        if "quit".startswith(cmd.split()[0].lower()):
+        self.push(''.join([cmd, '\r\n\r\n']))
+        if 'quit'.startswith(cmd.split()[0].lower()):
             self.close()
             return False
         else:
@@ -374,31 +329,32 @@ class _ClientHandler(asynchat.async_chat):
     def __init__(self, connections, mplayer, conn, log):
         asynchat.async_chat.__init__(self, conn)
         self.add_channel(connections)
-        self.set_terminator("\r\n\r\n")
+        self.set_terminator('\r\n\r\n')
         self.connections = connections
         self.mplayer = mplayer
-        # hook handle_mplayer_data method to mplayer
-        self.mplayer._server_data_handlers[self._fileno] = self.handle_mplayer_data
+        # hook handle_mplayer_data method to mplayer's stdout
+        self.handler_id = self.mplayer.stdout.add_handler(self.handle_mplayer_data)
         self.log = log
         self.buffer = []
 
     def handle_close(self):
+        self.mplayer.stdout.remove_handler(self.handler_id)
         del self.connections[self._fileno]
         self.close()
-        self.log("Connection closed: %s" % (self.addr, ))
+        self.log('Connection closed: %s' % (self.addr, ))
 
     def collect_incoming_data(self, data):
         self.buffer.append(data)
 
     def found_terminator(self):
-        data = "".join(self.buffer)
+        data = ''.join(self.buffer)
         self.buffer = []
         self.handle_data(data)
 
     def handle_data(self, data):
-        if not data or "quit".startswith(data.split()[0].lower()):
+        if not data or 'quit'.startswith(data.split()[0].lower()):
             self.handle_close()
-        elif data.lower() == "reload":
+        elif data.lower() == 'reload':
             # then restart the MPlayer process;
             self.mplayer.restart()
         else:
@@ -406,21 +362,63 @@ class _ClientHandler(asynchat.async_chat):
 
     def handle_mplayer_data(self, data):
         if data.startswith('ANS_'):
-            self.push("".join([data, "\r\n"]))
+            self.push(''.join([data, '\r\n']))
+
+
+class _File(object):
+    """Wrapper for stdout and stderr
+
+    Exposes the fileno() and readline() methods
+    Provides methods for management of data handlers
+    """
+
+    def __init__(self):
+        self._callbacks = {}
+        self._file = None
+
+    def _set_file(self, file):
+        self._unset_file()
+        self._file = file
+        _ReadableFile(asyncore.socket_map, file.fileno(), self.readline)
+
+    def _unset_file(self):
+        if self._file is not None:
+            del asyncore.socket_map[self._file.fileno()]
+        self._file = None
+
+    def fileno(self):
+        if self._file is not None:
+            return self._file.fileno()
+
+    def readline(self):
+        if self._file is not None:
+            data = self._file.readline().rstrip()
+            for handler in self._callbacks.values():
+                handler(data)
+            return data
+
+    def add_handler(self, callback):
+        uid = hash(callback)
+        self._callbacks[uid] = callback
+        return uid
+
+    def remove_handler(self, uid):
+        del self._callbacks[uid]
 
 
 if __name__ == '__main__':
     import sys
+    import signal
+
+    def handle_data(data):
+        print 'mplayer: ', data
 
     player = MPlayer()
     player.args = sys.argv[1:]
-    player.start(pipe_stdout=True)
-    def callback():
-        data = player.stdout.readline()
-        player.handle_data(data)
-    print add_output_watch(player.stdout, callback)
-    # this shouldn't have any effect since stderr wasn't PIPEd
-    print add_output_watch(player.stderr, callback)
+    player.stdout.add_handler(handle_data)
+    player.start()
+
+    signal.signal(signal.SIGTERM, lambda s, f: player.stop())
     try:
         asyncore.loop()
     except KeyboardInterrupt:
