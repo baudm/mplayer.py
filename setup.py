66654397bb3f54cce472a68f9f04429d64787ff3
Darwin Bautista <daruuin@gmail.com>
2010-07-05 14:57:21 +0000

    Update setup script

--- setup.py
+++ setup.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python
-# $Id: setup.py 361 2008-01-24 11:01:55Z darwin $
 
 from distutils.core import setup
 
-from pymplayer import __version__
+from mplayer import __version__
 
 
 setup(
     name='PyMPlayer',
     version=__version__,
-    description='Thin, out-of-source wrapper and client/server for MPlayer',
+    description='Lightweight, out-of-source wrapper for MPlayer',
     author='Darwin M. Bautista',
     author_email='djclue917@gmail.com',
-    url='http://unplug.eee.upd.edu.ph/pymplayer/',
-    license='GPL & LGPL',
-    py_modules=['pymplayer'],
-    platforms=['POSIX'],
+    url='http://code.google.com/p/python-mplayer/',
+    packages=['mplayer'],
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Environment :: Console :: Curses',
-        'Environment :: No Input/Output (Daemon)',
-        'Environment :: X11 Applications',
         'Environment :: X11 Applications :: GTK',
+        'Environment :: X11 Applications :: Qt',
         'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
         'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
-        'Topic :: Internet',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
         'Topic :: Multimedia :: Video :: Display',
-        'Topic :: Software Development :: Libraries :: Python Modules']
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
