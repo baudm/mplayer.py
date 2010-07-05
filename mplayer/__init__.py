2c76b5d4ab758dfe361d2dcfedd5e8f031284ea1
Darwin Bautista <daruuin@gmail.com>
2010-07-05 11:25:28 +0000

    __init__: use the same pre-conditions (used in asyncore) for AsyncMPlayer

--- mplayer/__init__.py
+++ mplayer/__init__.py
@@ -5,21 +5,22 @@
 Classes:
 
 MPlayer -- provides a basic and low-level interface to MPlayer
-AsyncMPlayer -- MPlayer subclass with asyncore integration (NOT for MS Windows)
-GtkMPlayer -- provides a very basic (as of now) GTK2 widget that embeds MPlayer
+AsyncMPlayer -- MPlayer subclass with asyncore integration (POSIX only)
+GtkMPlayer -- provides a basic (as of now) PyGTK widget that embeds MPlayer
+QtMPlayer -- provides a PyQt4 widget similar to GtkMPlayer in functionality
 """
 
 
 from mplayer.core import MPlayer
 from mplayer.gtk2 import GtkMPlayer
 from mplayer.qt4 import QtMPlayer
-import sys
-if sys.platform != 'win32':
+import os
+if os.name == 'posix':
     from mplayer.async import AsyncMPlayer
 
 
 __version__ = '0.5.0'
 __author__ = 'Darwin M. Bautista <djclue917@gmail.com>'
 __all__ = ['MPlayer', 'GtkMPlayer', 'QtMPlayer']
-if sys.platform != 'win32':
+if os.name == 'posix':
     __all__.append('AsyncMPlayer')
