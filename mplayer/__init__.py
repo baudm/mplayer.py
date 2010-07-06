e9350b1cbd8fbaceb8a5bf14a964a2e530191b0b
Darwin Bautista <daruuin@gmail.com>
2010-07-06 16:07:42 +0000

    __init__: import PIPE, STDOUT from subprocess for convenience

--- mplayer/__init__.py
+++ mplayer/__init__.py
@@ -8,12 +8,20 @@ MPlayer -- provides a basic and low-level interface to MPlayer
 AsyncMPlayer -- MPlayer subclass with asyncore integration (POSIX only)
 GtkMPlayer -- provides a basic (as of now) PyGTK widget that embeds MPlayer
 QtMPlayer -- provides a PyQt4 widget similar to GtkMPlayer in functionality
+
+Constants:
+
+PIPE -- subprocess.PIPE, provided here for convenience
+STDOUT -- subprocess.STDOUT, provided here for convenience
 """
 
 __version__ = '0.5.0'
 __author__ = 'Darwin M. Bautista <djclue917@gmail.com>'
-__all__ = ['MPlayer']
+__all__ = ['PIPE', 'STDOUT', 'MPlayer']
 
+
+# Import here for convenience.
+from subprocess import PIPE, STDOUT
 
 from mplayer.core import MPlayer
 try:
